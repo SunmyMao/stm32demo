2096684d97f408d48ca583b7d922b13d59830095
sunmy <xidanxiao_liu@163.com>
2018-11-26 23:17:31 +0800

    move code to engine

--- usr-src/steering_engine.c
+++ usr-src/steering_engine.c
@@ -1,9 +1,88 @@
 #include "steering_engine.h"
 
-void StreeingEngine_positiveRotate(StreeingEngine* o)
+void delay_x_ms(int x)
 {
+	int i,j;
+	for(i=0; i < x; i++)
+	{
+		for(j=0; j < 112; j++);
+	}
 }
 
-void StreeingEngine_reverseRotate(StreeingEngine* o)
+void StreeingEngine_init(StreeingEngine* o, GPIO_TypeDef* GPIOx, uint32_t RCC_APB2Periph, uint16_t a, uint16_t b, uint16_t c, uint16_t d)
 {
+	o->gpio_x = GPIOx;
+	o->gpio_pin_a = a;
+	o->gpio_pin_b = b;
+	o->gpio_pin_c = c;
+	o->gpio_pin_d = d;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph, ENABLE);
+	o->GPIO_InitStructure.GPIO_Pin = o->gpio_pin_a | o->gpio_pin_b | o->gpio_pin_c | o->gpio_pin_d;
+	o->GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	o->GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(o->gpio_x, &o->GPIO_InitStructure);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a | o->gpio_pin_b | o->gpio_pin_c | o->gpio_pin_d);
+}
+
+void StreeingEngine_clockwise(StreeingEngine* o, int speed)
+{
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+	
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+	
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+		
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+}
+
+void StreeingEngine_counterClockwise(StreeingEngine* o, int speed)
+{
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+	
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+	
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+
+	GPIO_SetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
+	delay_x_ms(speed);
+}
+
+void StreeingEngine_stop(StreeingEngine* o)
+{
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_a);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_b);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_c);
+	GPIO_ResetBits(o->gpio_x, o->gpio_pin_d);
 }
--- usr-src/steering_engine.h
+++ usr-src/steering_engine.h
@@ -1,12 +1,25 @@
 #ifndef _STEERING_ENGINE_H
 #define _STEERING_ENGINE_H
 
+#include "stm32f10x_gpio.h"
+
 typedef struct StreeingEngine
 {
-	int a;
+	GPIO_TypeDef* gpio_x;
+	uint16_t gpio_pin_a;
+	uint16_t gpio_pin_b;
+	uint16_t gpio_pin_c;
+	uint16_t gpio_pin_d;
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
 } StreeingEngine;
 
-void StreeingEngine_positiveRotate(StreeingEngine* o);
-void StreeingEngine_reverseRotate(StreeingEngine* o);
+void StreeingEngine_init(StreeingEngine* o, GPIO_TypeDef* GPIOx, uint32_t RCC_APB2Periph, uint16_t a, uint16_t b, uint16_t c, uint16_t d);
+
+void StreeingEngine_clockwise(StreeingEngine* o, int speed);
+void StreeingEngine_counterClockwise(StreeingEngine* o, int speed);
+void StreeingEngine_stop(StreeingEngine* o);
+
+void delay_x_ms(int x);
 
 #endif
